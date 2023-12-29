23aebba1415fea61e3d0b0af3d60a2692e633d01
tlee152 <tlee152@ford.com>
2023-12-29 16:11:55 -0500

    update script

--- container/raspberry_pi/docker_run_pi.sh
+++ container/raspberry_pi/docker_run_pi.sh
@@ -1,6 +1,6 @@
 # TODO: if container already exists, restart the container
-CONTAINER_NAME = "robot_dev"
-IMAGE = "iamtae/jammy-ros2-humble"
+export CONTAINER_NAME="robot_dev"
+export IMAGE="iamtae/jammy-ros2-humble"
 docker run -it -d -v /home:/home --privileged --name ${CONTAINER_NAME} ${IMAGE}
 info "Entering Docker Container ${CONTAINER_NAME} with Docker Image ${IMAGE}"
-docker exec -it robotics /bin/bash
+docker exec -it ${CONTAINER_NAME} /bin/bash
